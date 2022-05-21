963e65e35552f8729dfb29b85fe7654c4a10f0b2
Hubert <hubert.rozplochowski@gmail.com>
2022-05-21 14:33:03 +0200

    GetUser decorator added

--- src/folders/folders.controller.ts
+++ src/folders/folders.controller.ts
@@ -1,36 +1,45 @@
-import {Body, Controller, Delete, Get, Param, Post, ValidationPipe} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post, UseGuards, ValidationPipe} from '@nestjs/common';
+import {AuthGuard} from "@nestjs/passport";
+import {GetUser} from "../auth/get-user.decorator";
+import {User} from "../auth/user.entity";
 import {FolderDto} from "./dto/folder.dto";
 import {FoldersService} from "./folders.service";
 import {Folder} from "./interfaces";
 
 @Controller('folders')
+@UseGuards(AuthGuard())
 export class FoldersController {
 
     constructor(private foldersService: FoldersService) {
     }
 
     @Get('/all')
-    getFolders(): Promise<Folder[]> {
+    getFolders(@GetUser() user: User): Promise<Folder[]> {
         return this.foldersService.getFolders();
     }
 
     @Get('/:id')
-    getFolder(@Param('id') id: number) {
+    getFolder(@Param('id') id: number,
+              @GetUser() user: User) {
 
     }
 
     @Post()
-    createFolder(@Body(ValidationPipe) folder: FolderDto): Promise<Folder> {
+    createFolder(
+        @Body(ValidationPipe) folder: FolderDto,
+        @GetUser() user: User): Promise<Folder> {
         return this.foldersService.saveFolder(folder);
     }
 
     @Post()
-    updateFolder(@Body(ValidationPipe) folder: FolderDto) {
+    updateFolder(@Body(ValidationPipe) folder: FolderDto,
+                 @GetUser() user: User) {
 
     }
 
     @Delete('/:id')
-    deleteFolder(@Param('id') id: number) {
+    deleteFolder(@Param('id') id: number,
+                 @GetUser() user: User) {
 
     }
 }
